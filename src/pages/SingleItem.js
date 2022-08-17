daba304a08bb6d51f8dc950cd44c08de6225ff5e
ozcanemre <ozcan.emre1608@gmail.com>
2022-08-17 12:59:17 +0200

    rate

--- src/pages/SingleItem.js
+++ src/pages/SingleItem.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Helmet } from 'react-helmet'
-import { Link, useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom';
+
 
 export default function SingleItem({product}) {
   const params= useParams()
@@ -9,7 +10,23 @@ export default function SingleItem({product}) {
   // eslint-disable-next-line
   const found = product.find(item=>{ return item.id == params.id})
  
+    const rate= found?.rating.rate;
+    const number= Math.round(rate)
+    
+    function getStars(rating) {
+      const stars = [];
+      for (let i = 0; i < 5; i++) {
+        if (rating - 1 < i) {
+          stars.push(<span>☆</span>);
+        } else {
+          stars.push(<span>★</span>);
+          
+        }
+      }
+      return stars;
   
+    }
+    
  
   return (
     <div className='single-item'>
@@ -19,16 +36,21 @@ export default function SingleItem({product}) {
      <h2>{found?.title}</h2>
      <figure>
      <img src={found?.image} alt="" />
-    <figcaption><p><b>rate:</b>{found?.rating.rate}</p></figcaption>
+    
      </figure>
 
      
      
       <p><b>count:</b>{found?.rating.count}</p>
+      <div className='rate'>
+        <h3>rate:</h3>
+       <span>{getStars(number)}</span>
+
+      </div>
      
      <b>description:</b>
      <p className='description'>{found?.description}</p>
-     <div>
+     <div className='price-category'>
       <p><b>price:</b>{found?.price}</p>
       <p><b>category:</b>{found?.category}</p>
      </div>
